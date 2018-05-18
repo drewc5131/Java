445989cc8db87776cdf02e760286461646030b47
garbage labaire pcs <31282527+drewc5131@users.noreply.github.com>
2018-05-18 11:06:57 -0400

    xd

--- src/com/finalproject/FinalProjectMain.java
+++ src/com/finalproject/FinalProjectMain.java
@@ -1,8 +1,21 @@
 package com.finalproject;
 
+import com.eclipsesource.json.JsonObject;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 public class FinalProjectMain {
     public static void main(String[] args){
         GUIMain gui = new GUIMain();
         gui.Start();
+
+        try (FileReader reader = new FileReader("planets.json")) {
+        }
+        catch (FileNotFoundException e){
+
+        }
+        JsonObject json = JsonObject.readFrom(data.);
     }
 }
