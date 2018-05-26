bf1df80cb9925a137a50039123dfc5b8356cd832
Drew Ciaramitaro Laptop <drew.ciaramitaro51@gmail.com>
2018-05-26 17:03:21 -0400

    i was el bored xd

---
+++ js/main.js
@@ -0,0 +1,11 @@
+function OnPressQuestions() {
+    document.getElementById('questionbtn').classList.add('questionBtnOutPlaying');
+    document.body.classList.add('backgroundChangeColor');
+    document.getElementById('jumbo').style.setProperty('font-size', '0', 'important')
+    document.getElementById('title').style.setProperty('font-size', '0', 'important')
+    setTimeout(OpenQuestions, 3500)
+}
+
+function OpenQuestions() {
+    window.location.href='questions.html'
+}
